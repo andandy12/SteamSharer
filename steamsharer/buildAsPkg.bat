0533de5ebcf9dff7d6c5dd2174e14fe059ac60e9
andandy12 <30943710+andandy12@users.noreply.github.com>
2022-01-10 18:50:27 -0500

    Adding pauses to the buildAsPkg error

--- steamsharer/buildAsPkg.bat
+++ steamsharer/buildAsPkg.bat
@@ -1,13 +1,13 @@
 @echo off
 echo. && echo Checking if node.js is installed && echo.
 call node -v
-if errorlevel 1 echo. && echo You must have node.js installed, install from https://nodejs.org/en/ && goto :EOF
+if errorlevel 1 echo. && echo You must have node.js installed, install from https://nodejs.org/en/ && pause && goto :EOF
 echo. && echo Checking if npm is installed && echo.
 call npm -v
 if errorlevel 1 echo. && echo You must have node package manager installed, install from https://docs.npmjs.com/downloading-and-installing-node-js-and-npm && goto :EOF
 echo. && echo Checking if pkg is installed && echo.
 call pkg -v
-if errorlevel 1 echo. && echo Package is not currently installed... install with "npm install -g pkg" or from https://www.npmjs.com/package/pkg && goto :EOF
+if errorlevel 1 echo. && echo Package is not currently installed... install with "npm install -g pkg" or from https://www.npmjs.com/package/pkg && pause && goto :EOF
 echo. && echo Installing all dependencies && echo.
 call npm i
 echo. && echo Fixing a dependency  && echo.
