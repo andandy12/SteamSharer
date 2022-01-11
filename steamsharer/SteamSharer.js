920536e8398549a8354ace73c93de2a931734f0e
andandy12 <30943710+andandy12@users.noreply.github.com>
2022-01-10 19:14:22 -0500

    Update SteamSharer.js

--- steamsharer/SteamSharer.js
+++ steamsharer/SteamSharer.js
@@ -1,86 +1,68 @@
-var args = process.argv.slice(2);
-var hasLoggedIn = false;
-
-//used in askQuestion
-const readline = require('readline'); 
-//used when logon event happens
-var SteamID = require('steamid'); // required to get accountID for checking if account is authed
-const computername = require('os').hostname();
+const args = process.argv.slice(2);
+const readline = require('readline');
+const SteamID = require('steamid'); // required to get accountID for checking if account is authed
 const VDF = require('vdf');
-var Uint64LE = require("int64-buffer").Uint64LE;
-//needed to communicate with steam
 const SteamUser = require('steam-user');
-const client = new SteamUser();
-//filesteam needed to get local files
 const fs = require('fs');
+//const computername = require('os').hostname();
+//var Uint64LE = require("int64-buffer").Uint64LE;
+
+
+const client = new SteamUser();
+
 
 const addField = (fieldsToAdd, field, fieldValue) => {
-	if(fieldValue !== undefined && fieldValue !== null)
+	if (fieldValue !== undefined && fieldValue !== null)
 		fieldsToAdd[field] = fieldValue;
 	return fieldsToAdd;
 };
 function askQuestion(query) {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    return new Promise(resolve => rl.question(query, ans => {
-        rl.close();
-        resolve(ans);
-    }));
-}
-function sleep(ms) {
-	return new Promise((resolve) => {
-	  setTimeout(resolve, ms);
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
 	});
-} 
+	return new Promise(resolve => rl.question(query, ans => {
+		rl.close();
+		resolve(ans);
+	}));
+}
 
-
-client.on('steamGuard', async function(domain, callback) {
+client.on('steamGuard', async function (domain, callback) {
 	console.log("Steam Guard code needed from email ending in " + domain);
 	var code = await askQuestion("Enter steamGuard from email: ");
 	callback(code);
 });
 // https://github.com/DoctorMcKay/node-steam-user/wiki/Family-Sharing Family Sharing Github Page
 
-client.on('loggedOn', async function(details, parental) {
-	hasLoggedIn = true;
+client.on('loggedOn', async function () {
 	console.log("\x1b[32mAccount successfully logged in\x1b[0m");
 
 	const LocSteam64 = client.steamID.getSteamID64();
-	console.log("\x1b[36mLocal SteamID64: ",LocSteam64,"\x1b[0m");
-	
-	//console.log(details);
-	//console.log(parental);
-	
-	
-
-	async function addSteamIdAsBorrower(SteamID64){
-		console.log("Adding SteamID64("+SteamID64+") to authorized borrowers");
+	console.log("\x1b[36mLocal SteamID64: ", LocSteam64, "\x1b[0m");
+	async function addSteamIdAsBorrower(SteamID64) {
+		console.log("Adding SteamID64(" + SteamID64 + ") to authorized borrowers");
 		client.addAuthorizedBorrowers(SteamID64);
-		if(await checkifAuthBorrower(SteamID64)){
-			console.log('\x1b[32m%s\x1b[0m',"Account has been successfully added as borrower");
+		if (await checkifAuthBorrower(SteamID64)) {
+			console.log('\x1b[32m%s\x1b[0m', "Account has been successfully added as borrower");
 			return SteamID64;
-		}else
-			console.log('\x1b[31m%s\x1b[0m',"Account was not added as an borrower");
+		} else
+			console.log('\x1b[31m%s\x1b[0m', "Account was not added as an borrower");
 	}
-	
-	async function checkifAuthBorrower(SteamID64){
+
+	async function checkifAuthBorrower(SteamID64) {
 		let sid = new SteamID(SteamID64);
-		let options = {};
-		addField(options,"includePending",true);
-		let borrowers = await client.getAuthorizedBorrowers(options);
-		if(JSON.stringify(borrowers).includes(sid.accountid))
+		let borrowers = await client.getAuthorizedBorrowers({ "includePending": true });
+		if (JSON.stringify(borrowers).includes(sid.accountid))
 			return true;
 		else
 			return false;
 	}
-	
+
 	// Attempt to authorize device for sharing -- we currently dont get local device token as these functions are broken
-	
+
 	//client.authorizeRemoteSharingDevice(computername, devTokenitem);
 	//client.authorizeLocalSharingDevice(computername);
-	
+
 	/*DeviceAuth.ClientAuthorizeLocalDeviceRequest({
 		device_description: computername,
 		owner_account_id: sid.accountid,
@@ -91,24 +73,22 @@ client.on('loggedOn', async function(details, parental) {
 		authed_device_token})//- The resulting device token
 	*/
 	// Accept authorization for the account and the token
-	
+
 	//console.log(await client.activateSharingAuthorization(borrowersSteam64, devTokenitem));
 	//console.log(await client.getAuthorizedSharingDevices());
 
 	var borrowersSteamID;
-	for(;;){
+	for (; ;) {
 		//Attempt to add borrower from steamid
-		var question = "";
-		if(borrowersSteamID)
+		let question = "";
+		if (borrowersSteamID)
 			question += `Press enter to authorize ${borrowersSteamID} or provide a new SteamID: `;
 		else
 			question += "\tSteamID for an account to auth: ";
-
-		input = await askQuestion(question);
-		if(input.length > 0)
+		let input = await askQuestion(question);
+		if (input.length > 0)
 			borrowersSteamID = input;
 		await addSteamIdAsBorrower(borrowersSteamID);
-		console.log();
 		/*//authorize local device token currently broken
 		console.log(await client.getAuthorizedSharingDevices()); // debug
 		console.log("Attempting to authorize device under token:",devTokenitem);
@@ -116,23 +96,21 @@ client.on('loggedOn', async function(details, parental) {
 		console.log(await client.getAuthorizedSharingDevices()); // debug
 		*/
 
-		await askQuestion("Press enter to remove authorization");
-		
+		await askQuestion("\nPress enter to remove authorization");
+
 		//console.log("Removing device auth from: ", devTokenitem); // I believe device auth is pointless to remove... the client will then not have to reattempt to authorize.
 		//await client.deauthorizeSharingDevice(devTokenitem);
 		console.log("Removing borrower auth from: ", borrowersSteamID);
 		await client.removeAuthorizedBorrowers([borrowersSteamID]);
-		if(await checkifAuthBorrower(borrowersSteamID))
-			console.log('\x1b[31m%s\x1b[0m',"Account was not removed as an borrower");
+		if (await checkifAuthBorrower(borrowersSteamID))
+			console.log('\x1b[31m%s\x1b[0m', "Account was not removed as an borrower\n");
 		else
-			console.log('\x1b[32m%s\x1b[0m',"Account was successfully removed as an borrower");
-		
-		console.log();
+			console.log('\x1b[32m%s\x1b[0m', "Account was successfully removed as an borrower\n");
 	}
 });
-  
-client.on('debug', async function(string) {
-	//console.log(string);
+/*
+client.on('debug', async function (str) {
+	console.log(str);
 });
 
 
@@ -147,14 +125,11 @@ Object.keys(AuthorizedDevices).forEach((authDevice)=>{
 	deviceToken.push(token2.toString(10));
 });
 var devTokenmf = 1,devTokenm = 0,devTokenitem;
-for (var i=0; i<deviceToken.length; i++)
-{
-	for (var j=i; j<deviceToken.length; j++)
-	{
+for (var i=0; i<deviceToken.length; i++){
+	for (var j=i; j<deviceToken.length; j++){
 		if (deviceToken[i] == deviceToken[j])
-		devTokenm++;
-		if (devTokenmf<=devTokenm)
-		{
+			devTokenm++;
+		if (devTokenmf<=devTokenm){
 			devTokenmf=devTokenm; 
 			devTokenitem = deviceToken[i];
 		}
@@ -166,32 +141,32 @@ for (var i=0; i<deviceToken.length; i++)
 //Local Device has been found
 */
 var sentryfile = fs.readFileSync("C:\\Program Files (x86)\\Steam\\config\\config.vdf").toString();
-try{
+try {
 	sentryfile = VDF.parse(sentryfile).InstallConfigStore.Software.Valve.Steam.SentryFile;
-}catch(e){
+} catch (e) {
 	throw 'SentryFile not found in local config.vdf';
 }
 const sentrybuffer = fs.readFileSync(sentryfile);
 client.setSentry(sentrybuffer);
 //console.log("\x1b[36m","Using Sentry: ", sentrybuffer,"\x1b[0m");
 var user_config;
-if(fs.existsSync('./account.json'))
+if (fs.existsSync('./account.json'))
 	user_config = JSON.parse(fs.readFileSync('./account.json').toString());
 else
-	user_config = {username: "",password: ""};
+	user_config = { username: "", password: "" };
 
+console.log("Press Ctrl+C to hault the program at any point!");
+if (args.length == 2) {
+	user_config.username = args[0];
+	user_config.password = args[1];
+}
 (async () => {
-	console.log("Press Ctrl+C to hault the program at any point!");
-	if(!hasLoggedIn){
-		if(args.length == 2){
-			user_config.username = args[0];
-			user_config.password = args[1];
-		}
-		if((user_config.username == "") || (user_config.password === "")){
-			user_config.username = await askQuestion("Username: ");
-			user_config.password = await askQuestion("Password: ");
-			fs.writeFileSync('./account.json',JSON.stringify(user_config));
-		}
-		client.logOn({"accountName": user_config.username,"password": user_config.password,"rememberPassword": true});
+	if ((user_config.username == "") || (user_config.password === "")) {
+
+		user_config.username = await askQuestion("Username: ");
+		user_config.password = await askQuestion("Password: ");
+		fs.writeFileSync('./account.json', JSON.stringify(user_config));
+
 	}
+	client.logOn({ "accountName": user_config.username, "password": user_config.password, "rememberPassword": true });
 })();
